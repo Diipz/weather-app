0a4847c2d4a4fe27bc008b3a54172b4db9cf243b
dipesh <dip_patel100@hotmail.com>
2022-12-11 14:25:15 +0000

    Combine HTML pages and remove unnecessary async function

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
-const searchBtn = document.querySelector("#search-icon");
+let searchBtn = document.querySelector("#search-icon");
 
 
-async function getCoordinates() {
-    try {
-        const location = "london"//document.querySelector("input").value;
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=c1549ca689f583a53da882e62424c483`, { mode: "cors" });
-        locationCoords = await response.json();
-        return locationCoords;
-    } catch (error) {
-        alert("Unknown location");
-    }
+if (searchBtn !== null) {
+    searchBtn.addEventListener("click", () => {
+
+        let city = document.getElementById("location").value;
+
+
+        if (city !== "") {
+            getWeatherData(city);
+            setTimeout(function () {
+                let content = document.querySelector(".content");
+                content.style.display = "none";
+                let results = document.querySelector(".result-screen");
+                results.style.display = "block";
+            }, 2000)
+
+        } else {
+            return alert("select a city");
+        }
+    })
 }
 
-async function getWeatherData() {
+
+
+async function getWeatherData(city) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${locationCoords[0].lat}&lon=${locationCoords[0].lon}&appid=c1549ca689f583a53da882e62424c483`, { mode: "cors" });
-        weatherData = await response.json();
-        return weatherData;
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c1549ca689f583a53da882e62424c483&units=metric`, { mode: "cors" });
+        data = await response.json();
+        createWeatherCard(data);
+        return;
+
     } catch (error) {
         alert(error);
     }
 }
 
-function createWeatherCard() {
+function createWeatherCard(weatherData) {
     let temp = document.querySelector("#result-temp");
     let place = document.querySelector("#result-place");
     let weather = document.querySelector("#result-weather");
@@ -33,15 +47,16 @@ function createWeatherCard() {
     let minTemp = document.querySelector("#result-min-temp");
     let maxTemp = document.querySelector("#result-max-temp");
 
-    temp.textContent = Math.round((weatherData.main.temp - 273.15) * 10) / 10 + "\u00B0";
+    temp.textContent = Math.round(weatherData.main.temp * 10) / 10 + "\u00B0";
     place.textContent = weatherData.name;
     weather.textContent = weatherData.weather[0].main;
     description.textContent = weatherData.weather[0].description;
-    feelsLike.textContent = Math.round((weatherData.main.feels_like - 273.15) * 10) / 10 + "\u00B0";
+    feelsLike.textContent = Math.round(weatherData.main.feels_like * 10) / 10 + "\u00B0";
     humidity.textContent = weatherData.main.humidity + "%";
     pressure.textContent = weatherData.main.pressure + "mb";
-    minTemp.textContent = Math.round((weatherData.main.temp_min - 273.15) * 10) / 10 + "\u00B0";
-    maxTemp.textContent = Math.round((weatherData.main.temp_max - 273.15) * 10 / 10) + "\u00B0";
-
+    minTemp.textContent = Math.round(weatherData.main.temp_min * 10) / 10 + "\u00B0";
+    maxTemp.textContent = Math.round(weatherData.main.temp_max * 10 / 10) + "\u00B0";
 }
+
+
 
